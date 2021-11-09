a46c7ebaf46ba59f3ff94d80c049e0a62095fb98
Stew Sabatino <stewsabatino@gmail.com>
2021-11-09 12:23:33 -0600

    added user, post, comment and like classes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,59 @@
 const User = require('./User');
+const Playlist = require('./Playlist');
+const Comment = require('./Comment');
+const Like = require('./Like')
 
-module.exports = { User };
+User.hasMany(Playlist, {
+    foreignKey: 'user_id',
+});
+
+Playlist.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+Playlist.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Playlist, {
+    foreignKey: 'post_id',
+});
+
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+Playlist.hasMany(Like, {
+    foreignKey: 'post_id',
+});
+
+Like.belongsTo(Playlist, {
+    foreignKey: 'post_id',
+});
+
+
+Comment.hasMany(Like, {
+    foreignKey: 'comment_id',
+});
+
+Like.belongsTo(Comment, {
+    foreignKey: 'comment_id',
+});
+
+
+User.hasMany(Like, {
+    foreignKey: 'user_id',
+});
+
+Like.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+module.exports = { User, Playlist, Comment, Like };
