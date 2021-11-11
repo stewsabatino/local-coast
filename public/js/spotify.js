605ccefbe5ab7135c2641ef46da729735088504b
Ian Clark <IanClark2021@u.northwestern.edu>
2021-11-11 16:44:38 -0600

    staged change commit

--- public/js/spotify.js
+++ public/js/spotify.js
@@ -4,7 +4,21 @@ const spotifyApi = new SpotifyWebApi();
 const playlistAddHandler = (event) => {
     const id = getElementById('add-to-home').getAttribute("value");
     const singlePlaylist = spotifyApi.getUserPlaylist(id);
-    return singlePlaylist;
+    singlePlaylist;
+    const response = await fetch('/api/users/login', { 
+            method: 'POST', 
+            body: JSON.stringify({ // body content as JSON 
+                email: email.value,
+                pass: pass.value, 
+                }), 
+            header: { 'Content-Type': 'application/json' }, 
+        });
+    if (response.ok) { 
+        document.location.replace('/dashboard');
+    } else {
+        alert(`Yuht-Oh, Login attempt failed`);
+        console.log(response);
+    }
 }
 
 document.getElementById('add-to-home').addEventListener('click', playlistAddHandler);
