ed4fa0047e2df083ea8f750e879de13a9086f60d
Ian Clark <IanClark2021@u.northwestern.edu>
2021-11-10 20:30:16 -0600

    console log removal

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const commentRoutes = require('./commentRoutes');
-// const dashboardRoutes = require('./dashboardRoutes');
 router.use('/comments', commentRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
